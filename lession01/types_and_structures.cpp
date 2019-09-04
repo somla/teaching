7efbc980d54ac7c76acebc7442a0e4c53292c535
somla <edesalmos@hotmail.com>
2019-09-05 00:12:30 +0200

    bugfix in types_and_structures.cpp

--- lession01/types_and_structures.cpp
+++ lession01/types_and_structures.cpp
@@ -73,11 +73,11 @@ int main()
     
     int matrix[2][7];
 
-    a[0][0] = 3;
+    matrix[0][0] = 3;
 
     std::vector<int> v(10);
 
-    std::vector< std::vector<int> > vmatrix(10, 10);
+    std::vector< std::vector<int> > vmatrix( 10, std::vector<int>(10) ) ;
 
 
     return 0;
